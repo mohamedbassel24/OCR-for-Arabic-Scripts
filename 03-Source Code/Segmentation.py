279c05b2ebc3e67658301bcd8cb1e03db9530c71
Mohamed Bassel <48108775+mohamedbassel24@users.noreply.github.com>
2019-11-28 12:03:18 +0200

    Lines Segementation in progress

--- 03-Source Code/Segmentation.py
+++ 03-Source Code/Segmentation.py
@@ -14,12 +14,23 @@ for filename in sorted(glob.glob('../02-Dataset/Training_Data/*.png')):
     th = 0
     # lines segmentation involves horizontal projection of the image rows to find the empty rows between rows that contain text a
 
-    SegemenetedRows = []
-    for i in range(thresh.shape[0]):
+    SegemenetedRows = np.zeros((800,thresh.shape[1]))
+    index=0
+    i=0
+    while  i < thresh.shape[0]:
         if np.sum(thresh[i, :, 0]) != thresh.shape[1] * 255:
-            SegemenetedRows.append(thresh[i])
-    IndexsOfLines =y for y in range(thresh.shape[0]) if np.sum(thresh[y, :, 0]) != thresh.shape[1] * 255
-    print("Dim: ", len(SegemenetedRows))
+            while(np.sum(thresh[i, :, 0]) != thresh.shape[1] * 255):
+                SegemenetedRows[index] = (thresh[i, :, 0])
+                index = index + 1
+                i=i+1
+            SegemenetedRows[index] = (np.ones(thresh.shape[1])) # LINE ENDEND
+            index = index + 1
+        i=i+1
+
+   # IndexsOfLines = y for y in range(thresh.shape[0]) if np.sum(thresh[y, :, 0]) != thresh.shape[1] * 255
+    print("Dim: ", np.shape(SegemenetedRows))
+    io.imshow(SegemenetedRows, cmap="binary")  # 0~255 np.zeros((2, 1))
+    io.show()
     cv2.namedWindow('image', cv2.WINDOW_NORMAL)
     cv2.imshow(filename, img)
     cv2.waitKey(0)
