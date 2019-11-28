1ac67751d046171af406069fecb7c3671de5f935
Mohamed Bassel <48108775+mohamedbassel24@users.noreply.github.com>
2019-11-28 21:23:56 +0200

    Main with Seperate Functions

--- 03-Source Code/Segmentation.py
+++ 03-Source Code/Segmentation.py
@@ -1,50 +1,40 @@
-from Preprocessing import *
-import sys
+from commonfunctions import *
 
-for filename in sorted(glob.glob('../02-Dataset/Training_Data/*.png')):
-    #filename = "csep1220.png"  # The Rotated Image
-    img = cv2.imread(filename)
-    img = Preprocess(img)
 
+def SegementedImageLines(img):
     img = 255 - img
     ret, thresh = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY_INV)
-    hist = cv2.reduce(thresh, 0, cv2.REDUCE_AVG).reshape(-1)
-    plt.hist(thresh.ravel(), 256, [0, 255])
-    plt.show()
-    th = 0
+    # hist = cv2.reduce(thresh, 0, cv2.REDUCE_AVG).reshape(-1)
+    #  plt.hist(thresh.ravel(), 256, [0, 255])
+    #  plt.show()
     # lines segmentation involves horizontal projection of the image rows to find the empty rows between rows that contain text a
 
     SegemenetedRows = np.zeros((800, thresh.shape[1]))
-    index = 0
-    i = 0
-    ListOfImageLines = []
-    while i < thresh.shape[0]:
-
+    index = 0  # for the whole image with lines
+    i = 0  # iteratior on the rows of image(NxM)
+    ListOfImageLines = []  # Contains the Segemented Lines Images
+    while i < img.shape[0]:
         if np.sum(thresh[i, :, 0]) != thresh.shape[1] * 255:
-            First_Point = i-1
-            while (np.sum(thresh[i, :, 0]) != thresh.shape[1] * 255):
+            First_Point = i - 1
+            while np.sum(thresh[i, :, 0]) != thresh.shape[1] * 255:
                 SegemenetedRows[index] = (thresh[i, :, 0])
                 index = index + 1
                 i = i + 1
-            LastPoint = i+2 # for ي
-            i=i+2
-            if abs(First_Point-LastPoint) <6:
-                continue # not a line :( points
-            partition = np.copy(img[First_Point:LastPoint, :, 0])
+            LastPoint = i + 2  # for ي
+            i = i + 2
+            if abs(First_Point - LastPoint) < 6:
+                continue  # not a line :( points
+            partition = np.copy(thresh[First_Point:LastPoint, :, 0])
             ListOfImageLines.append(partition)
-            SegemenetedRows[index] = (np.ones(thresh.shape[1]))  # LINE ENDEND
+            SegemenetedRows[index] = (np.ones(thresh.shape[1]))  # LINE END
             index = index + 1
         else:
             i = i + 1
 
     for LineImage in ListOfImageLines:
-
         io.imshow(LineImage, cmap="binary")  # 0~255 np.zeros((2, 1))
         io.show()
-    # IndexsOfLines = y for y in range(thresh.shape[0]) if np.sum(thresh[y, :, 0]) != thresh.shape[1] * 255
-    print("Dim: ", np.shape(SegemenetedRows))
-    io.imshow(SegemenetedRows, cmap="binary")  # 0~255 np.zeros((2, 1))
+    ImageWithLine = SegemenetedRows[0:index, :]
+    io.imshow(ImageWithLine, cmap="binary")  # 0~255 np.zeros((2, 1))
     io.show()
-    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-    cv2.imshow(filename, img)
-    cv2.waitKey(0)
+    return ListOfImageLines
---
+++ 03-Source Code/main.py
@@ -0,0 +1,11 @@
+from Preprocessing import *
+from Segmentation import *
+
+
+
+for filename in sorted(glob.glob('../02-Dataset/Training_Data/*.png')):
+    #filename = "csep1220.png"  # The Rotated Image
+    img = cv2.imread(filename)
+    img = Preprocess(img)
+    SegementedLines=[]
+    SegementedLines=SegementedImageLines(img)
