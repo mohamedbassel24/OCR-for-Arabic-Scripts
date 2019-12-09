e63e0e125d3d4084b3c60f157e0196406265ee42
MarwanMedhat97 <marwanmedhatgamal@gmail.com>
2019-12-09 15:07:14 +0200

    completing feature extraction

--- 03-Source Code/Feature Extraction.py
+++ 03-Source Code/Feature Extraction.py
@@ -2,7 +2,7 @@ from commonfunctions import *
 from scipy import ndimage
 
 
-def findLetterContourArea(img):
+def findLetterContourArea(img): 
     # This function finds the contours of a given image and returns it in the variable contours.
     # This function will not work correctly unless you preprocess the image properly as indicated.
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -45,9 +45,60 @@ def Height_Width_Ratio(img):
     #print(w)
     return h/w,   area/whiteArea
 
+def Max_transition_rows(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, img = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY)
 
-image=io.imread("test character letters/qaaff.png")
-h_w,r=Height_Width_Ratio(image)
+    MaxTransition = 0
+   # MaxTransitionIndex = Base_INDEX
+    for i in range(img.shape[0]):  # loop on Each row
+        CurrTransitionRow = 0
+        flag = 1
+        for j in range(img.shape[1]):  # loop on coloumns for specific row
+            if flag == 1 and img[i, j] == 0:
+                flag = 0
+                CurrTransitionRow += 1
+            elif flag == 0 and img[i, j] == 255:
+                flag = 1
+                CurrTransitionRow += 1
 
-print(h_w)
-print(r)
+        if CurrTransitionRow >= MaxTransition:
+            #MaxTransitionIndex = i
+            MaxTransition = CurrTransitionRow
+    return MaxTransition
+
+def Max_transition_colomns(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, img = cv2.threshold(gray, 50, 255, cv2.THRESH_BINARY)
+
+    MaxTransition = 0
+    # MaxTransitionIndex = Base_INDEX
+    for i in range(img.shape[1]):  # loop on each coloumn
+        CurrTransitionCol = 0
+        flag = 1
+        for j in range(img.shape[0]):  # loop on rows for specific coloumn
+            if flag == 1 and img[j, i] == 0:
+                flag = 0
+                CurrTransitionCol += 1
+            elif flag == 0 and img[j, i] == 255:
+                flag = 1
+                CurrTransitionCol += 1
+
+        if CurrTransitionCol >= MaxTransition:
+            # MaxTransitionIndex = i
+            MaxTransition = CurrTransitionCol
+    return MaxTransition
+
+
+
+img=io.imread("test character letters/heh.png")
+#h_w,r=Height_Width_Ratio(image)
+show_images([img])
+#print (img)
+
+#print (img)
+x = Max_transition_rows(img)
+y = Max_transition_colomns(img)
+
+print(x)
+print(y)
